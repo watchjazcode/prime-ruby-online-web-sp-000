0ae3c600a74da3eb72d4dd12473608a3f775cb02
Jazmin M <jazminmerckel@gmail.com>
2019-10-03 22:36:56 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,13 @@
 # Add code here!
 def prime?(integer)
-  if integer <= 1 
+  if integer <= 1 # checks for negative numbers
     return false
   end
-  numbers = (2..(integer - 1)).to_a  
+  #range and converts string to array
+  numbers = (2..(integer - 1)).to_a
+  # iterates over each number in array
   numbers.each do |number|
+#checks if number is divides into others besides 1 & itself
     if integer % number == 0
       return false
     end 
