f0c5f34ddb49f7c0028b29f7b1ef006a0ebb3a9a
Jazmin M <jazminmerckel@gmail.com>
2019-10-03 22:02:09 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
-# Add  code here!
+# Add code here!
+def prime(integer)
+  number = (2..(integer - 1).to_a
+  if number %  !== 0
+    return true
+  end
+end
+
+
+
+collection = (2..integer - 1).to_a
+my_find(collection) {|i| i % 3 == 0 and i % 5 == 0 }
+
+def prime(integer)
+  i = 0
+  while i < integer.length
+    return collection[i] if yield(collection[i])
+    i = i + 1
+  end
+end
